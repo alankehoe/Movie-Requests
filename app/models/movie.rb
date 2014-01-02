660b084a9f464fd915859a162d379193c4f47e84
Alan Kehoe <alankehoe111@gmail.com>
2014-01-02 18:59:38 +0000

    glyphicon's

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ActiveRecord::Base
   #validates_presence_of :name
-  #validates_uniqueness_of :name, :message => 'This movie already exists'
+  validates_uniqueness_of :name, :message => 'This movie already exists'
 end
