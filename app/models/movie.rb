be3c3dd78e4948b5b9899fd48c47acb2bd966446
Alan Kehoe <alankehoe111@gmail.com>
2014-01-03 13:17:08 +0000

    better validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,11 @@
 class Movie < ActiveRecord::Base
-  #validates_presence_of :name
+  validates_presence_of :name
   validates_uniqueness_of :name, :message => 'This movie already exists'
+
+  before_validation :upcase_name
+
+  private
+  def upcase_name
+    self.name = self.name.upcase if self.name.present?
+  end
 end
